4adf5463047f984180371655a177f90aa87b9f7a
Woo <jiwoopark727@gmail.com>
2024-08-22 13:26:02 +0900

    vite환경 React기반 타입스크립트 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const App = () => {
+  return (
+    <>
+      <div>App Component</div>
+    </>
+  )
 }
+export default App;
