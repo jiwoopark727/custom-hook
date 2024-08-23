56496f1582fc407dac601ceed51566a019dc1f9e
Woo <jiwoopark727@gmail.com>
2024-08-23 13:46:23 +0900

    커스텀 훅 제작 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import useInput from './hooks/useInput';
+
+
 const App = () => {
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const [name, onChangeName] = useInput("");
+
+
   return (
     <>
-      <div>App Component</div>
+      <pre>
+      {
+        JSON.stringify({email, password, name}, null, 2)
+      }
+      </pre>
+
+      <input type="email" value={email} onChange={onChangeEmail}/>
+      <input type="password" value={password} onChange={onChangePassword}/>
+      <input type="text" value={name} onChange={onChangeName}/>
+      <input type="date"/>
     </>
   )
 }
